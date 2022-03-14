80253693c1f316a9021b301a241065aa28dbc068
anagomezcr <amgomez179@misena.edu.co>
2022-03-14 08:08:35 -0500

    Se realiza aplicacion para calcular la mediana de salarios en colombia, la mediana de salarios del 10% de los salarios general.

---
+++ media_aritmetica.js
@@ -0,0 +1,44 @@
+function mediaAritmetica() {
+/*   let sumavalores = 0;
+
+for (let i = 0; i < valores.length; i++) {
+    sumavalores = sumavalores + valores[i];*/
+const sumavalores = valores.reduce(
+    function (valorAcumulado = 0, nuevoElemento){
+        
+return valorAcumulado + nuevoElemento;
+    })
+const promediovalores = sumavalores / valores.length;
+ return promediovalores;
+}
+const valores = [];
+
+//calcular media aritmetica, para cualquier valores
+
+function onClickButtonListMediaAritmetica() {
+    const input1 = document.getElementById("InputValue1")
+    const value1 = input1.value;
+    const numero = valores.unshift(Number(value1));
+
+    mediaAritmetica()
+    const resultC = document.getElementById("resultC")
+    resultC.innerText = "Tu valores de numeros es: " + valores;
+}
+
+function onClickButtonMediaAritmetica() {
+    const input1 = document.getElementById("InputValue1")
+    const value1 = input1.value;
+    
+    const resultD = document.getElementById("resultD")
+    resultD.innerText = "La media armonica es: " + mediaAritmetica();
+}
+
+function onClickButtonRevMediaAritmetica() {
+    const input1 = document.getElementById("InputValue1")
+    const value1 = input1.value;
+    const numero = valores.shift(Number(value1));
+
+    mediaAritmetica()
+    const resultC = document.getElementById("resultC")
+    resultC.innerText = "Tu valores de numeros es: " + valores;
+}
